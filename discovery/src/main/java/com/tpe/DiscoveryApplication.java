17b16645e14cdb7dc56c1bf18646bef09c3afcad
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-02-03 23:15:21 +0200

    Car Service

--- discovery/src/main/java/com/tpe/DiscoveryApplication.java
+++ discovery/src/main/java/com/tpe/DiscoveryApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscoveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
-
 }
--- log-service/src/main/java/com/tpe/LogServiceApplication.java
+++ log-service/src/main/java/com/tpe/LogServiceApplication.java
@@ -8,5 +8,4 @@ public class LogServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LogServiceApplication.class, args);
 	}
-
 }
